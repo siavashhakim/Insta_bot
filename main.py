e2f051b27f1df21b8dae608c1b284515a8041254
siavash <siavash.hakimelahi@crc.com>
2021-03-25 20:27:28 -0700

    version 1.1.0

--- main.py
+++ main.py
@@ -3,13 +3,6 @@
 By Siavash Hakim Elahi
 
 """
-from time import sleep
-from selenium import webdriver
-
-browser = webdriver.Firefox()
-browser.get('https://www.instagram.com/')
-sleep(5)
-browser.close()
 
 
 

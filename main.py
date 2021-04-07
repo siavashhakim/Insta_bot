078362e8fb18ac5b5cdf2006e3d4324f166520db
siavash <siavash.hakimelahi@crc.com>
2021-04-06 19:03:44 -0700

    version 1.1.1

--- main.py
+++ main.py
@@ -1,5 +1,18 @@
 """
 # Instagram bot
+
+In this work, I make an insta bot with python
+
+What do SocialCaptain, Kicksta, Instavast, and many other companies have in common?
+They all help you reach a greater audience, gain more followers, and get more likes on Instagram while
+you hardly lift a finger. They do it all through automation, and people pay them a good deal of money for it.
+But you can do the same thing—for free—using InstaPy!
+
+How Instagram bots work
+How to automate a browser with Selenium
+How to use the Page Object Pattern for better readability and testability
+How to build an Instagram bot with InstaPy
+
 By Siavash Hakim Elahi
 
 """

a20b116e23e08803b2ef1472fc7fb7ac0f6fbb08
siavash <siavash.hakimelahi@crc.com>
2021-04-28 16:50:54 -0700

    version 2.0.3 - insta rel

--- mian_relationship.py
+++ mian_relationship.py
@@ -1,10 +1,30 @@
 from instapy import InstaPy
 import pandas as pd
+from datetime import datetime
+import os
 
 input = pd.read_csv('input_credentials.csv')
 
 session = InstaPy(username=input.Username[0], password=input.Password[0], headless_browser=True)
 session.login()
 
-user_following = session.grab_following(username="30avashhe", amount="full")
+user_following = session.grab_following(username="", amount="full")
 ##now, `lazySmurf_following` variable which is a list- holds the `Following` data of "lazy.smurf" at requested time
+
+today_date = datetime.now()
+d_time = today_date.strftime("%d-%m-%Y")
+
+d = {'followers': user_following}
+output = pd.DataFrame(data=d)
+output.to_csv('list_follower' + d_time + '.csv')
+
+
+# difference --> new follower
+main_dir = os.getcwd()
+new_path = main_dir + str('\\'+'list_follower_old.csv')
+old_list = pd.read_csv(new_path)
+
+diff_list_new = set(output.followers.tolist()) - set(old_list.followers.tolist())
+
+diff_list_lost = set(old_list.followers.tolist()) - set(output.followers.tolist())
+

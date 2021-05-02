54722facbb8621654f0d5ddffd7effebd13336a5
siavash <siavash.hakimelahi@crc.com>
2021-05-02 15:20:24 -0700

    version 2.0.3 - insta rel2

--- mian_relationship.py
+++ mian_relationship.py
@@ -8,7 +8,7 @@ input = pd.read_csv('input_credentials.csv')
 session = InstaPy(username=input.Username[0], password=input.Password[0], headless_browser=True)
 session.login()
 
-user_following = session.grab_following(username="", amount="full")
+user_following = session.grab_following(username="pa", amount="full")
 ##now, `lazySmurf_following` variable which is a list- holds the `Following` data of "lazy.smurf" at requested time
 
 today_date = datetime.now()
